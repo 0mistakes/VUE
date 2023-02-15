4b55f903212ee6d2979019bce697c6a5ea4e827f
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 20:44:02 +0300

    34 quest

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,20 @@ export default {
         },
     ],
     items: [1, -2, 3, -4, 5],
+    products: [
+			{
+				id: 1,
+				name: 'product1',
+			},
+			{
+				id: 2,
+				name: 'product2',
+			},
+			{
+				id: 3,
+				name: 'product3',
+			},
+		]
 	};
 },
 
@@ -129,6 +143,9 @@ export default {
   <div v-for="item in items">
     <p v-if="item >= 0">{{ item }}&nbsp;</p>
   </div>
+  <ul v-for="smh in products" :key="smh.id">
+    <li>{{ smh.name }}</li>
+  </ul>
 </template>
 <style >
 
