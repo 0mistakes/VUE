b820503701a231cb12a8381c936536163892c222
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:14:12 +0300

    43 quest

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,17 @@ export default {
     amount: 5,
     isVisible: false,
     visible: [false, false, false],
-    obj: {
-        hidden: true,
-      },
-    task: "Task",
-    task_number: 1,
+    number: 1,
+    number_square: "Task 1",
+    numero_uno: 0,
+    numero_dos: 0,
+    sum_number: "Task 2",
+    text_uno: "placeholder",
+    text_dos: "placeholder",
+    fullName: "",
+    userName: "",
+    userSurname: "",
+    userPatronymic: "",
 	};
 },
 
@@ -27,29 +33,46 @@ export default {
     },
   
   methods: {
-    setDone: function () {
-      this.obj.hidden = !this.obj.hidden;
+    square: function () {
+      this.number_square = this.number ** 2;
     },
-    show() {
-      alert(new Date().toLocaleString());
+    sum: function () {
+      this.sum_number = this.numero_uno + this.numero_dos;
     },
-    handleClick() {
-      this.clicks++;
-      console.log('Button clicked');
+    swap: function () {
+      const temp = this.text_uno;
+      this.text_uno = this.text_dos;
+      this.text_dos = temp;
+    },
+    split: function () {
+      this.userSurname = this.fullName.split(" ")[0];
+      this.userName = this.fullName.split(" ")[1];
+      this.userPatronymic = this.fullName.split(" ")[2];
     },
   }
 };
 </script>
 <template>
-  <input type="text" v-model="task" />
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <p>{{ task }}</p>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <p>{{ task.toUpperCase() }}</p>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <input type="number" v-model="task_number" />
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <p>{{ task_number ** 2 }}</p>
+  input type="number" v-model="number" />
+  <button @click="square">Square of number</button>
+  &nbsp;
+  <p>{{ number_square }}</p>
+  &nbsp;
+  <input type="number" v-model="numero_uno" />
+  <input type="number" v-model="numero_dos" />
+  <button @click="sum">Summ of numbers</button>
+  &nbsp;
+  <p>{{ sum_number }}</p>
+  &nbsp;
+  <input type="text" v-model="text_uno" />
+  <input type="text" v-model="text_dos" />
+  <button @click="swap">Swap values</button>
+  &nbsp;
+  <input type="text" v-model="fullName" />
+  <button @click="split">Split full name</button>
+  <p>Surname = {{ userSurname }}</p>
+  <p>Name = {{ userName }}</p>
+  <p>Patronymic = {{ userPatronymic }}</p>
 </template>
 <style >
 .active {
