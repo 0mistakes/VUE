808f2d49279a2b0c61c7471080cf27bf224a674e
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:33:37 +0300

    53 quest

--- src/App.vue
+++ src/App.vue
@@ -22,18 +22,9 @@ export default {
         "Saturday",
         "Sunday",
       ],
-      dayYear: "",
-      dayYearOptions: [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-      ],
-      monthYear: "",
-      monthYearOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      yearYear: "",
-      yearYearOptions: [
-        2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,
-        2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000,
-      ],
+      var_1: "Task 1",
+      newItem: "",
+      items: ["a", "b", "c", "d", "e"],
     };
   },
   computed: {
@@ -43,28 +34,24 @@ export default {
     },
   
   methods: {
-    todayDate: function () {
-      var today = new Date();
-      this.dayYear = String(today.getDate());
-      this.monthYear = String(today.getMonth() + 1);
-      this.yearYear = today.getFullYear();
+    addItemEnd: function () {
+      this.items.push(this.newItem);
+    },
+    addItemBegin: function () {
+      this.items.unshift(this.newItem);
     },
   }
 };
 </script>
 <template>
-   <input
-    type="checkbox"
-    :checked="isDisabled"
-    @click="($event) => (this.isDisabled = !this.isDisabled)"
-  />
-  &nbsp;
-  <button
-    v-bind:disabled="!isDisabled"
-    @click="($event) => (this.isDisabled = !this.isDisabled)"
-  >
-    {{ isDisabled ? "input is enabled" : "input is disabled" }}
-  </button>
+   <input v-model="newItem" />
+  <button @click="addItemEnd">add to the end</button>
+  <button @click="addItemBegin">add to the beginning</button>
+  <ul v-for="(item, index) in items" :key="index">
+    <li>
+      {{ item }}
+    </li>
+  </ul>
 </template>
 <style >
 .active {
