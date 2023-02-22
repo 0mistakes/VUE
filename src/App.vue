996affe42514bf92258d8c028f96b68e920925e5
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:09:51 +0300

    41 quest

--- src/App.vue
+++ src/App.vue
@@ -109,7 +109,13 @@ export default {
   <button @mouseenter="show" class="custom-btn btn-12">
     <span>Узнать дату</span>
   </button>
-  <p :class="{ active: isValid, valid: isDisabled }">Task 1</p>
+  <p :style="{ color: 'green', background: 'yellow', fontSize: '30px' }">
+    Task 1
+  </p>
+  &nbsp; &nbsp; &nbsp;
+  <p :style="{ fontWeight: 'bold', fontStyle: 'italic', fontSize: '30px' }">
+    Task 2
+  </p>
 </template>
 <style >
 .active {
