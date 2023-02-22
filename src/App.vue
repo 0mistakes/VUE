472257a75cde3351a72c4ebdc6ba523ad421b0f7
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:28:34 +0300

    50 quest

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,28 @@ export default {
     cost: 10,
     amount: 5,
     dayWeek: "Task 1",
-    dayWeekOptions: [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ],
-    dayYear: "",
-    dayYearOptions: [
+    dayWeek: "Task 1",
+      dayWeekOptions: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      dayYear: "",
+      dayYearOptions: [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-    ],
-    monthYear: "",
-    monthYearOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    yearYear: "",
-    yearYearOptions: [
+      ],
+      monthYear: "",
+      monthYearOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      yearYear: "",
+      yearYearOptions: [
         2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,
         2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000,
-    ],
+      ],
     };
   },
   computed: {
@@ -51,9 +52,7 @@ export default {
 };
 </script>
 <template>
-  &nbsp;
-  <p>{{ selected }}</p>
-  <select v-model="dayWeek">
+   <select v-model="dayWeek">
     <option v-for="option in dayWeekOptions">{{ option }}</option>
   </select>
   &nbsp;
