0ed6a09b1b636982d34ca9b6524ce0792f695026
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-03-01 17:36:55 +0300

    57 quest

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,12 @@
 <script>
+
 export default {
-    data() {
-	return {
-    developers: [
-        {
-          id: 1,
-          name: "Bebr",
-          salary: 100,
-          age: 30,
-          isEdit: false,
-        },
-        {
-          id: 2,
-          name: "Bebroid",
-          salary: 200,
-          age: 40,
-          isEdit: false,
-        },
-        {
-          id: 3,
-          name: "Diand",
-          salary: 300,
-          age: 50,
-          isEdit: false,
-        },
-      ],
-    };
+  components: {
+    Employee,
   },
-  computed: {
-      price() {
-        return this.cost * this.amount;
-      },
-    },
-  
-  methods: {
-    removeItem: function (id) {
-      this.developers = this.developers.filter((developer) => {
-        return developer.id !== id;
-      });
-    },
-    edit: function (developer) {
-      developer.isEdit = true;
-    },
-    save: function (developer) {
-      developer.isEdit = false;
-    },
-  }
 };
 </script>
 <template>
-   <table>
-    <tr>
-      <th>ID</th>
-      <th>Имя</th>
-      <th>Зарплата</th>
-      <th>Возраст</th>
-    </tr>
-    <tr v-for="developer in developers">
-      <template v-if="!developer.isEdit">
-        <td>{{ developer.id }}</td>
-        <td>{{ developer.name }}</td>
-        <td>{{ developer.salary }}</td>
-        <td>{{ developer.age }}</td>
-        <td><button @click="removeItem(developer.id)">remove</button></td>
-        <td><button @click="edit(developer)">edit</button></td>
-      </template>
-      <template v-else>
-        <td><input v-model="developer.id" /></td>
-        <td><input v-model="developer.name" /></td>
-        <td><input v-model="developer.salary" /></td>
-        <td><input v-model="developer.age" /></td>
-        <td><button @click="save(developer)">save</button></td>
-      </template>
-    </tr>
-  </table>
 </template>
 <style >
 .active {
---
+++ src/components/Employee.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+name: "0error"
+        }
+    }
+}
+</script>
+
+<template>
+   <p class="developer">{{ name }}</p>
+</template>
+
+<style>
+</style>
